0374b6eacf05a3c6a26f58faaa6ee3c840aa143a
milenoss <milenkhanal@gmail.com>
2019-11-28 11:33:44 +0000

    Done

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SearchBar = () => { 
+
+
+
+    return (
+        <form>
+            <label>
+                SearchBar
+                <input type="text" name="name" />  
+            </label>
+            <input type='submit' value= 'Submit'/>
+        </form>
+
+    )
+}
