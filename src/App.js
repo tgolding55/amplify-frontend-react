e3e4f09a842e25e57b465851b28542e0926d62fb
Tim Golding <tgolding55@hotmail.com>
2019-11-28 11:45:50 +0000

    Added spotify auth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
+import SpotifyLogin from "react-spotify-login";
+import { clientId, redirectUri } from "./settings";
+import "./App.css";
+import API from "./adapters/API";
+import ShowContainer from "./containers/ShowContainer";
 
-import './App.css';
-import API from './adapters/API'
-import ShowContainer from './containers/ShowContainer'
 function App() {
+  const makeQuery = () => {
+    API.fetchSongQuery().then(songs => setSongs(songs));
+  };
 
-const makeQuery = () => { 
-  API.fetchSongQuery()
-  .then(songs => setSongs(songs))
-}
+  const [songs, setSongs] = useState([]);
 
-const [songs, setSongs] = useState([])
+  const onFailure = () => {
+    console.log("FAILURE");
+  };
 
+  const onSuccess = () => {
+    console.log("SUCCESS");
+  };
 
+  useEffect(
+    makeQuery,
 
-useEffect(
-
-makeQuery,
-
-[]);
+    []
+  );
 
   return (
     <div className="App">
-      <ShowContainer songs = {songs}/>
+      <SpotifyLogin
+        clientId={clientId}
+        redirectUri={redirectUri}
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+      />
+      <ShowContainer songs={songs} />
     </div>
   );
 }
---
+++ src/settings.js
@@ -0,0 +1,2 @@
+export const clientId = "2ba1b7bb2dd9448ab9ae4c7614d95728";
+export const redirectUri = "http://localhost:3000";
